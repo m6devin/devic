f8284e2a83317b3d8958a069a5e5f19599f1c750
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-06-26 00:32:28 +0430

    Create translate route and controller

---
+++ app/Http/Controllers/TranslateController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TranslateController extends Controller
+{
+    public function translate(Request $r) {
+        
+    }
+}
