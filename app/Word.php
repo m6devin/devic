b37ca9578a86c891a4600c2579283165c0f38913
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-06-26 02:37:06 +0430

    Bug fix + load translations of user

--- app/Word.php
+++ app/Word.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Word extends Model
 {
@@ -18,7 +19,18 @@ class Word extends Model
      * @return Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function translations () {
-        return $this->hasMany(Word::class, "word_id");
+        return $this->hasMany(Translation::class, "word_id");
     }
+
+    /**
+     * Get all translations of word inserted by authenticated user
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function myTranslations () {
+        return $this->hasMany(Translation::class, "word_id")->where("created_by_id", Auth::user()->id);
+    }
+
+
 
 }
