743308a07113f161300ccfbe0353976df7c35348
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-06-29 03:37:41 +0430

    Remove useless functions

--- app/Word.php
+++ app/Word.php
@@ -22,28 +22,6 @@ class Word extends Model
         return $this->hasMany(Translation::class, "word_id");
     }
 
-    /**
-     * Get all translations of word inserted by authenticated user
-     *
-     * @return Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function myTranslations () {
-        return $this
-                ->hasMany(Translation::class, "word_id")
-                ->where("created_by_id", Auth::user()->id);
-    }
-
-    /**
-     * Get all translations of word inserted by authenticated user
-     * @param string $alpha2code 
-     * @return Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function myTranslationsToLanguage (string $alpha2code) {
-        return $this
-        ->hasMany(Translation::class, "word_id")
-        ->where("created_by_id", Auth::user()->id);
-    }
-
     public function language() {
         return $this->belongsTo(Language::class);
     }
