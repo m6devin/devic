1d013f99ef552e6b0553bd86dea335a392bbbc57
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-06-29 03:48:18 +0430

    Change translation home page

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
+Route::get('/', 'TranslateController@translate')->name('translate.translate')->middleware('auth');
 Route::group(["prefix" => "translation", "middleware" => "auth"], function() {
-    Route::get('/translate', 'TranslateController@translate')->name('translate.translate');
     Route::post('/word/save', 'TranslateController@saveWord')->name('translate.saveWord');
     Route::post('/translate/save', 'TranslateController@saveTranslation')->name('translate.save');
 });
