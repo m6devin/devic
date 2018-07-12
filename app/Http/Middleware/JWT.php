c44f0394145f6900179814e9127b0c2bc1bc43fb
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-07-12 22:32:22 +0430

    Handle token exceptions

--- app/Http/Middleware/JWT.php
+++ app/Http/Middleware/JWT.php
@@ -16,12 +16,29 @@ class JWT {
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        $user = JWTAuth::parseToken()->authenticate();
-        
-        if(!$user) {
+        try{
+            $user = JWTAuth::parseToken()->authenticate();
+            
+            if(!$user) {
+                return response()->json([
+                    'message' => 'Unauthorized'
+                ], 401);
+            }
+        }  catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
             return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
+                'message'   => 'Token Expired'
+            ], 401);                    
+        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json([
+                'message'   => 'Invalid Token'
+            ], 401);                    
+        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json([
+                'message'   => 'Token absent'
+            ], 401);                    
         }
 
         return $next($request);
