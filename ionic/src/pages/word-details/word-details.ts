6dbac191e260fe4a9385492b2bd094773a8f29c5
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-07-13 17:19:52 +0430

    Go to edit page of translation from details page Listen to change event

--- ionic/src/pages/word-details/word-details.ts
+++ ionic/src/pages/word-details/word-details.ts
@@ -1,25 +1,57 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the WordDetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
+import { TranslationSavePage } from '../translation-save/translation-save';
+import { TranslationService } from '../../app/services/translation.service';
+import * as _ from 'lodash';
 
 @IonicPage()
 @Component({
   selector: 'page-word-details',
   templateUrl: 'word-details.html',
+  providers: [TranslationService]
 })
-export class WordDetailsPage {
+export class WordDetailsPage implements OnInit {
+  word: any = null;
+  basicInfo: any = {};
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public translationService: TranslationService,
+    public events: Events) {
+    this.word = this.navParams.get('word');
+    if (this.word == null) {
+      this.navCtrl.pop();
+    }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.events.subscribe('translation:save', translation => {
+      let i = _.findIndex(this.word.translations, item => {
+        return item.id == translation.id;
+      })
+
+      if (i == -1) {
+        return;
+      }
+
+      this.word.translations[i] = translation;
+    });
+  }
+
+  ngOnInit() {
+    this.translationService.getBasicInfo().subscribe(res => { this.basicInfo = res }, err => { });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad WordDetailsPage');
+  }
+
+  /**
+   * Call translation from page to create on update a translation
+   * @param translation any If you want to add a new translation, pass null otherwise
+   * If you want edit an existing translation, pass translation object with ID
+   */
+  translationSave(translation: any = null){
+    this.navCtrl.push(TranslationSavePage, {
+      translation: translation,
+      word: this.word,
+      basicInfo: this.basicInfo,
+    });
   }
 
 }
