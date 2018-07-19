ac959d44bc793d8974a96c6fcb08da69e10ab9f5
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-07-20 02:13:18 +0430

    Handle back key

--- ionic/src/app/app.component.ts
+++ ionic/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform, App } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, App, Nav, IonicApp, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -12,6 +12,7 @@ import { ConfigPage } from '../pages/config/config';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav:Nav;
 
   pages = [
     { label: 'Translation', component: TranslationDashboardPage },
@@ -24,21 +25,72 @@ export class MyApp {
   constructor(platform: Platform,
     public app: App,
     statusBar: StatusBar,
-    splashScreen: SplashScreen) {
-      platform.ready().then(() => {
-        // Okay, so the platform is ready and our plugins are available.
-        // Here you can do any higher level native things you might need.
-        statusBar.styleDefault();
-        splashScreen.hide();
-      });
+    splashScreen: SplashScreen,
+    private _ionicApp: IonicApp,
+    private _menu: MenuController) {
+    platform.ready().then(() => {
+      history.pushState(null, null, '');
+      statusBar.styleDefault();
+      splashScreen.hide();
+      this.setupBackButtonBehavior();
+
+    });
 
   }
 
 
   openPage(page) {
+    this.nav.setRoot(page.component);
+  }
 
-    let nav =this.app.getActiveNavs()
-    nav[0].setRoot(page.component);
+  private setupBackButtonBehavior() {
+    // If on web version (browser)
+    if (window.location.protocol !== "file:") {
+
+      // Register browser back button action(s)
+      window.onpopstate = (evt) => {
+        // Close menu if open
+        if (this._menu.isOpen()) {
+          this._menu.close();
+          return;
+        }
+
+        // Close any active modals or overlays
+        let activePortal = this._ionicApp._loadingPortal.getActive() ||
+          this._ionicApp._modalPortal.getActive() ||
+          this._ionicApp._toastPortal.getActive() ||
+          this._ionicApp._overlayPortal.getActive();
+
+        if (activePortal) {
+          activePortal.dismiss();
+          return;
+        }
+
+        switch (localStorage.getItem('lastPage')) {
+          case "WordDetailsPage":
+            if (this.nav.canGoBack()) this.nav.pop();
+            return false;
+          default:
+            console.log(history.length);
+            return false;
+        }
+
+      };
+
+      // Fake browser history on each view enter
+      this.app.viewDidEnter.subscribe((app) => {
+        localStorage.setItem('lastPage', app.name);
+        switch (app.name) {
+          case "WordDetailsPage":
+            history.pushState(null, null, '');
+            return false;
+          default:
+            break;
+        }
+      });
+
+    }
+
   }
 }
 
