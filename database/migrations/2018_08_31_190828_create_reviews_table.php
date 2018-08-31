f192c181f4ae24edd832b5e86fc6b558c569d3e6
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-08-31 23:41:58 +0430

    Create review table and model

---
+++ database/migrations/2018_08_31_190828_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("word_id")->unsigned()->index();
+            $table->boolean('remembered');
+            $table->timestamps();
+
+            $table->foreign("word_id")
+            ->on("words")
+            ->references("id")
+            ->onDelete("CASCADE")
+            ->onUpdate("CASCADE");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
