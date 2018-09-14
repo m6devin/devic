e3ad9d071a6c627ee49161b6fe3c07cfa1e7ffe9
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-09-14 23:35:54 +0430

    Add step_id to review table

--- database/migrations/2018_08_31_190828_create_reviews_table.php
+++ database/migrations/2018_08_31_190828_create_reviews_table.php
@@ -1,39 +1,39 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateReviewsTable extends Migration
-{
+class CreateReviewsTable extends Migration {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer("word_id")->unsigned()->index();
+            $table->integer('word_id')->unsigned()->index();
+            $table->integer('step_id')->unsigned()->nullable();
             $table->boolean('remembered');
             $table->timestamps();
 
-            $table->foreign("word_id")
-            ->on("words")
-            ->references("id")
-            ->onDelete("CASCADE")
-            ->onUpdate("CASCADE");
+            $table->foreign('word_id')
+            ->on('words')
+            ->references('id')
+            ->onDelete('CASCADE')
+            ->onUpdate('CASCADE');
+
+            $table->foreign('step_id')
+            ->on('steps')
+            ->references('id')
+            ->onDelete('CASCADE')
+            ->onUpdate('CASCADE');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('reviews');
     }
 }
