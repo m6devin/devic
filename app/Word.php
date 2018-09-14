eb58b947b3893d8bb82c7ad1410b78f6f3b7c434
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-09-14 23:22:19 +0430

    Add step relation

--- app/Word.php
+++ app/Word.php
@@ -3,23 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Auth;
 
-class Word extends Model
-{
+class Word extends Model {
     protected $fillable = [
-        "word",
-        "created_by_id",
-        "language_id"
+        'word',
+        'created_by_id',
+        'language_id',
     ];
 
     /**
-     * Get all translations of word
+     * Get all translations of word.
      *
      * @return Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function translations () {
-        return $this->hasMany(Translation::class, "word_id");
+    public function translations() {
+        return $this->hasMany(Translation::class, 'word_id');
     }
 
     public function language() {
@@ -30,5 +28,10 @@ class Word extends Model
         return $this->hasMany(Review::class)->orderBy('id', 'desc');
     }
 
-
+    /**
+     * Get current step of Leitner review.
+     */
+    public function step() {
+        return $this->belongsTo(Step::calss);
+    }
 }
