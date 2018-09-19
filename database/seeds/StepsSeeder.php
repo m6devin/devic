7a440c62ca2f56892d6f43f096ce7e94c2b2b440
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-09-20 00:27:26 +0430

    Update steps seeder

--- database/seeds/StepsSeeder.php
+++ database/seeds/StepsSeeder.php
@@ -9,13 +9,11 @@ class StepsSeeder extends Seeder {
      */
     public function run() {
         $steps = [
-            ['id' => 1, 'name' => 'New word', 'days' => 0],
-            ['id' => 2, 'name' => 'Step 1', 'days' => 1],
-            ['id' => 3, 'name' => 'Step 2', 'days' => 2],
-            ['id' => 4, 'name' => 'Step 3', 'days' => 4],
-            ['id' => 5, 'name' => 'Step 4', 'days' => 8],
-            ['id' => 6, 'name' => 'Step 5', 'days' => 16],
-            ['id' => 7, 'name' => 'Archived', 'days' => 0],
+            ['id' => 1, 'name' => 'Step 1', 'days' => 1],
+            ['id' => 2, 'name' => 'Step 2', 'days' => 2],
+            ['id' => 3, 'name' => 'Step 3', 'days' => 4],
+            ['id' => 4, 'name' => 'Step 4', 'days' => 8],
+            ['id' => 5, 'name' => 'Step 5', 'days' => 16],
         ];
 
         foreach ($steps as $v) {
