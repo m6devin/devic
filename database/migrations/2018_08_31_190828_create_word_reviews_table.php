3be6218b40cc8eec0d3e57020b354d5c3f992cd2
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2019-05-23 11:07:34 +0430

    Rename mode and migration of reviews to word_reviews

--- database/migrations/2018_08_31_190828_create_word_reviews_table.php
+++ database/migrations/2018_08_31_190828_create_word_reviews_table.php
@@ -4,12 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReviewsTable extends Migration {
+class CreateWordReviewsTable extends Migration {
     /**
      * Run the migrations.
      */
     public function up() {
-        Schema::create('reviews', function (Blueprint $table) {
+        Schema::create('word_reviews', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('word_id')->unsigned()->index();
             $table->integer('step_id')->unsigned()->nullable();
