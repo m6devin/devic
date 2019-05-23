a951842af3c50c0aded0108ad95f9c036df39b19
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2019-05-23 13:40:39 +0430

    Rename back the review Add review_type

--- app/Review.php
+++ app/Review.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class WordReview extends Model
+class Review extends Model
 {
     public function word() 
     {
--- database/migrations/2018_08_31_190828_create_reviews_table.php
+++ database/migrations/2018_08_31_190828_create_reviews_table.php
@@ -4,23 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWordReviewsTable extends Migration {
+class CreateReviewsTable extends Migration {
     /**
      * Run the migrations.
      */
     public function up() {
-        Schema::create('word_reviews', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('word_id')->unsigned()->index();
+            $table->string('review_type', 2)->commet('w: review words to remember translations; t: review translations to remember words');
+            $table->integer('items_id')->unsigned();
             $table->integer('step_id')->unsigned()->nullable();
             $table->boolean('remembered');
             $table->timestamps();
-
-            $table->foreign('word_id')
-            ->on('words')
-            ->references('id')
-            ->onDelete('CASCADE')
-            ->onUpdate('CASCADE');
 
             $table->foreign('step_id')
             ->on('steps')
